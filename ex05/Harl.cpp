a0a16a44ebf3ac8931070f242d08c1487ff3d00d
Myeonghyeon Park <mypark@c1r2s6.42seoul.kr>
2022-05-21 15:45:52 +0900

    ex05

---
+++ ex05/Harl.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mypark <mypark@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 13:09:31 by mypark            #+#    #+#             */
+/*   Updated: 2022/05/21 15:06:30 by mypark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl() {
+  PrintMsg[0] = &Harl::none;
+  PrintMsg[1] = &Harl::debug;
+  PrintMsg[2] = &Harl::info;
+  PrintMsg[3] = &Harl::warning;
+  PrintMsg[4] = &Harl::error;
+}
+
+Harl::~Harl() {}
+
+void Harl::none() {
+  std::cout << std::setw(7) << std::left << "NONE"
+            << ": "
+            << "Wrong level." << std::endl;
+}
+
+void Harl::debug() {
+  std::cout << std::setw(7) << std::left << "DEBUG"
+            << ": "
+            << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup "
+               "burger. I really do!"
+            << std::endl;
+}
+void Harl::info() {
+  std::cout << std::setw(7) << std::left << "INFO"
+            << ": "
+            << "I cannot believe adding extra bacon costs more money. You didn't putenough bacon "
+               "in my burger! If you did, I wouldn't be asking for more!"
+            << std::endl;
+}
+void Harl::warning() {
+  std::cout << std::setw(7) << std::left << "WARNING"
+            << ": "
+            << "I think I deserve to have some extra bacon for free. I've been coming foryears "
+               "whereas you started working here since last month."
+            << std::endl;
+}
+
+void Harl::error() {
+  std::cout << std::setw(7) << std::left << "ERROR"
+            << ": "
+            << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level) {
+  int msg;
+
+  msg = (level == "DEBUG") * 1 + (level == "INFO") * 2 + (level == "WARNING") * 3 +
+        (level == "ERROR") * 4;
+  (this->*PrintMsg[msg])();
+}
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*   test.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mypark <mypark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/21 12:13:11 by mypark            #+#    #+#             */
-/*   Updated: 2022/05/21 15:05:03 by mypark           ###   ########.fr       */
+/*   Created: 2022/05/21 14:55:53 by mypark            #+#    #+#             */
+/*   Updated: 2022/05/21 15:00:31 by mypark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef Harl_HPP
-#define Harl_HPP
-#include <iomanip>
-#include <iostream>
+#include "Harl.hpp"
 
-class Harl {
- private:
-  void none(void);
-  void debug(void);
-  void info(void);
-  void warning(void);
-  void error(void);
-  void (Harl::*PrintMsg[5])();
+int main(void) {
+  Harl harl;
 
- public:
-  Harl();
-  ~Harl();
-  void complain(std::string level);
-};
+  harl.complain("DEBUG");
+  harl.complain("Debug");
+  harl.complain("debug");
 
-#endif  // Harl_HPP
+  harl.complain("INFO");
+  harl.complain("Info");
+  harl.complain("info");
+
+  harl.complain("ERROR");
+  harl.complain("Error");
+  harl.complain("error");
+
+  harl.complain("WARNING");
+  harl.complain("Warning");
+  harl.complain("warning");
+}
